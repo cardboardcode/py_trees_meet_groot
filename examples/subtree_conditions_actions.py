da9ff3401bc81fbb2e8eb9a12cae2b62c2c092f1
Gary Bey <beyhy94@gmail.com>
2026-08-08 00:55:18 +0800

    :hammer: Updated subtree_conditions_actions.py to utilise abstracted condition node.
    Signed-off-by: Gary Bey <beyhy94@gmail.com>

--- examples/subtree_conditions_actions.py
+++ examples/subtree_conditions_actions.py
@@ -1,41 +1,11 @@
 import py_trees
 import time
-import uuid
 import sys
 
 from py_trees_meet_groot import groot_xml
 from nodes.bt_action_node import PrintMessage
+from nodes.bt_condition_node import DoesBlackboardValueExist
 
-
-class DoesBlackboardValueExist(py_trees.behaviour.Behaviour):
-    def __init__(self, **kwargs):
-        allowed_keys = {"ID", "key_name"}
-
-        for key, value in kwargs.items():
-            if key not in allowed_keys:
-                raise ValueError(f"Unknown parameter: {key}")
-            setattr(self, key, value)
-
-        self.key_name = getattr(self, "key_name", "Insert key_name here.")
-
-        name = f"{self.__class__.__name__}_{uuid.uuid4().hex[:4]}"
-        super().__init__(name)
-
-        self.blackboard = py_trees.blackboard.Client(name=name)
-        self.blackboard.register_key(key=self.key_name, access=py_trees.common.Access.READ)
-
-    def update(self):
-        print(f"Checking for blackboard key: [{self.key_name}]...")
-
-        try:
-            if hasattr(self.blackboard, self.key_name):
-                print(f"[INFO] - Blackboard key [{self.key_name}] does exist...")
-                return py_trees.common.Status.SUCCESS
-            else:
-                return py_trees.common.Status.FAILURE
-        except KeyError as error:
-            print(f"[ERROR] - {error}")
-            return py_trees.common.Status.FAILURE
 
 if __name__ == "__main__":
     all_behaviors = [PrintMessage, DoesBlackboardValueExist]
